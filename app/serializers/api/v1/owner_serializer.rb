511e40ad2616db4561e670e33635ee2cabee33b9
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-11-23 14:53:20 +0500

    Update owner and tenant serializers keys.

--- app/serializers/api/v1/owner_serializer.rb
+++ app/serializers/api/v1/owner_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::OwnerSerializer < ActiveModel::Serializer
   attributes :id
-  has_one :user
+  has_one :user, key: :account
   has_one :apartment
   has_one :ownership
 end
--- app/serializers/api/v1/tenant_serializer.rb
+++ app/serializers/api/v1/tenant_serializer.rb
@@ -1,6 +1,6 @@
 class Api::V1::TenantSerializer < ActiveModel::Serializer
   attributes :id
-  has_one :user
+  has_one :user, key: :account
   has_one :owner
   has_one :tenantship
 end
