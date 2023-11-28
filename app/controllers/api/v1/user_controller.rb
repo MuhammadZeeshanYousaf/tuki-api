6378689e2e614e05cc3a9fedaa6a3b93786dd25e
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-11-28 18:37:01 +0500

    Fix error raise Zeitwerk::NameError.new(msg, cref.last).

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Users::UserController < Api::V1::BaseController
+class Api::V1::UserController < Api::V1::BaseController
 
   def info
     render json: current_api_v1_user, serializer: UserSerializer
