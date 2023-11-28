b3f3b2fdb5201fc06bc2e0bcb0c722cc1e963f93
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-11-28 18:44:19 +0500

    Fix model premitive method key word "valid?" issue.

--- app/models/validation.rb
+++ app/models/validation.rb
@@ -1,5 +1,5 @@
 class Validation < ApplicationRecord
-  enum :status, { valid: 0, not_valid: 1 }
+  enum :status, { valid_user: 0, invalid_user: 1 }
 
   belongs_to :booking
   belongs_to :validated_by, class_name: 'User'
