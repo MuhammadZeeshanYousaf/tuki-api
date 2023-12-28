3408bd66a84c0bb4e870fb7a395fcb393c56520a
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-12-28 14:19:08 +0500

    Update guest.rb

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -6,7 +6,7 @@ class Guest < ApplicationRecord
   belongs_to :invited_by, class_name: 'User'
   belongs_to :approved_by, class_name: 'User', optional: true
   validates :user, presence: true, uniqueness: { message: 'Guest already exist' }
-  after_create :attach_qr_image
+  after_create :attach_qr_image, :send_guest_email
 
   def is_valid?
     valid_from <= DateTime.current && valid_to >= DateTime.current if valid_from.present? && valid_to.present?
@@ -16,5 +16,11 @@ class Guest < ApplicationRecord
 
     def qr_image_data
       "#{ENV['HOST_URL']}#{validate_api_v1_guest_path(id)}"
+    end
+
+  private
+
+    def send_guest_email
+      # @Todo - get guest details and send QR code email to guest
     end
 end
