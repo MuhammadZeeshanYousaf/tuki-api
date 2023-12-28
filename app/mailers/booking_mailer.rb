286471d1376a00f2aae424ecdfc9e61239f80d21
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-12-28 14:15:31 +0500

    Update booking mailer.

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -3,8 +3,10 @@ class BookingMailer < ApplicationMailer
   def booking_details
     @attendee = params[:attendee]
     @user = @attendee.user
-
-    # @Todo - extract booking details
+    @booking = @attendee.booking
+    @booker = @booking.booker
+    @time_slot = @booking.time_slot
+    @event = @time_slot.event
 
     mail(to: email_address_with_name(@user.email, @user.name), subject: "Booking invitation - #{APP_NAME}!")
   end
