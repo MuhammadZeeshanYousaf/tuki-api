d91deb5c9a5cb640641d3c6790eaebddccdd5396
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-12-28 14:11:28 +0500

    Add day validation in time slot.

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -15,16 +15,40 @@ class TimeSlot < ApplicationRecord
   validates :start_time, presence: true, uniqueness: { scope: [ :event_id, :day ] }
   validates :end_time, presence: true, uniqueness: { scope: [ :event_id, :day ] }
   validate :start_and_end_time_uniqueness
+  validate :day_in_range
 
   default_scope { order(created_at: :desc) }
 
   before_create :set_available_seats
+
+  def date_of_the_day
+    slot_event = self.event
+    this_date = slot_event.start_date
+    max_days = 30
+    day_count = 1
+
+    while this_date != slot_event.end_date.next_day && day_count != max_days do
+      if this_date.strftime("%A").downcase.eql? day.downcase
+        return this_date
+      else
+        this_date = this_date.next_day
+      end
+      day_count += 1
+    end
+    false
+  end
 
   private
 
     def start_and_end_time_uniqueness
       if start_time == end_time
         errors.add(:base, 'Start time and end time must be unique')
+      end
+    end
+
+    def day_in_range
+      unless date_of_the_day.present?
+        errors.add(:day, 'must exist in the event date range!')
       end
     end
 
